d9c635150dee56da7dda3af7c95d5789822a9062
sa556693828 <sa556693828@gmail.com>
2023-07-10 18:45:05 +0800

    neww

--- src/components/Prizes/Prizes.tsx
+++ src/components/Prizes/Prizes.tsx
@@ -1,67 +1,44 @@
 import React from "react";
 import Section from "../Section/Section";
 import PrizesCard from "./Card";
-import title from "@/assets/prizes.svg";
-import style from "@/assets/dotThree.svg";
-import Filecoin from "@/assets/Prizes/Filecoin.svg";
-import Lead from "@/assets/Prizes/Lead.png";
-import Wormhole from "@/assets/Prizes/wormhole.png";
-import bingX from "@/assets/Prizes/bingX.png";
-import bingxVe from "@/assets/Prizes/bingxVe.png";
-import huobi from "@/assets/Prizes/huobi.png";
-import chaintimes from "@/assets/Prizes/chaintimes.png";
-import Orderly from "@/assets/Prizes/Orderly.svg";
+import sideBgT from "@/assets/sideBgT.svg";
+import sideBg from "@/assets/sideBg.svg";
 
-export default function Prizes() {
+export default function Prizes(props: { data: any }) {
+  const { data } = props;
   return (
-    <Section
-      title={{ title: "Prizes", color: "red" }}
-      subTitle="獎金"
-      id="Prizes"
-      style={{ pic: style.src, w: "90" }}
-    >
-      <div className="grid w-full grid-cols-1 gap-5 px-4 md:grid-cols-3">
-        <PrizesCard
-          title="Orderly"
-          prize="$10,000"
-          pic={{ pic: Orderly.src, w: "50" }}
-        />
-        <PrizesCard
-          title="Lead Capital"
-          prize="$5,000"
-          pic={{ pic: Lead.src, w: "50" }}
-        />
-        <PrizesCard
-          title="Wormhole"
-          prize="$2,000"
-          pic={{ pic: Wormhole.src, w: "50" }}
-        />
-        <PrizesCard
-          title="FileCoin"
-          prize="$2,000"
-          pic={{ pic: Filecoin.src, w: "50" }}
-        />
-        <PrizesCard
-          title="BingX Ventures"
-          prize="$ -"
-          pic={{ pic: bingxVe.src, w: "50" }}
-        />
-        <PrizesCard
-          title="BingX"
-          prize="$ -"
-          pic={{ pic: bingX.src, w: "50" }}
-        />
-        <PrizesCard
-          title="Huobi Ventures"
-          prize="$ -"
-          pic={{ pic: huobi.src, w: "50" }}
-        />
-        <PrizesCard
-          title="Chaintimes Ventures"
-          prize="$ -"
-          pic={{ pic: chaintimes.src, w: "50" }}
-        />
+    <>
+      <img src={sideBgT.src} alt="TimeLine" className="w-full" />
+      <div className="flex w-full flex-col items-center justify-center gap-10 pt-32">
+        <div className="text-[2rem] uppercase">Total Prize</div>
+        <div className="h-32 w-4/5 rounded-2xl bg-gradient-to-r from-yellowF to-yellowT p-[2px] shadow-yellowLi transition-all">
+          <div
+            className={`flex h-full w-full items-center justify-center gap-6 rounded-2xl bg-bg p-4 md:gap-2`}
+          >
+            <div className="bg-gradient-to-r from-yellowF to-yellowT bg-clip-text p-2 text-5xl text-transparent">
+              {data.totalPrize}
+            </div>
+          </div>
+        </div>
       </div>
-    </Section>
+      <Section
+        title={{ title: data.title.name, color: data.title.color }}
+        subTitle={data.subTitle}
+        id={data.id}
+      >
+        <div className="grid w-full grid-cols-1 gap-5 px-4 md:grid-cols-3">
+          {data?.data.map((item: any, idx: any) => (
+            <PrizesCard
+              key={idx}
+              title={item.title}
+              prize={item.prize}
+              pic={{ pic: item.pic, w: "50" }}
+              content={item.content}
+            />
+          ))}
+        </div>
+      </Section>
+      <img src={sideBg.src} alt="TimeLine" className="w-full" />
+    </>
   );
 }

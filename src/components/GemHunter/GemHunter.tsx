2d3320a7c36679b3a16e874931635601384a35eb
sa556693828 <sa556693828@gmail.com>
2023-07-13 21:45:45 +0800

    fix

--- src/components/GemHunter/GemHunter.tsx
+++ src/components/GemHunter/GemHunter.tsx
@@ -12,6 +12,17 @@ export default function GemHunter(props: { data: any }) {
         subTitle={data.subTitle}
         id={data.id}
       >
+        <div className="flex w-full flex-col items-center justify-center py-12">
+          <div className="h-32 w-4/5 rounded-2xl bg-gradient-to-r from-yellowF to-yellowT p-[2px] shadow-yellowLi transition-all">
+            <div
+              className={`flex h-full w-full items-center justify-center gap-6 rounded-2xl bg-bg p-4 md:gap-2`}
+            >
+              <div className="bg-gradient-to-r from-yellowF to-yellowT bg-clip-text p-2 text-5xl text-transparent">
+                {data.totalPrize}
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="grid w-full grid-cols-1 gap-5 px-4 md:grid-cols-3">
           {data?.data.map((item: any, idx: any) => (
             <HunterCard
@@ -22,17 +33,6 @@ export default function GemHunter(props: { data: any }) {
               data={item.data}
             />
           ))}
-        </div>
-        <div className="flex w-full flex-col items-center justify-center pt-12">
-          <div className="h-32 w-4/5 rounded-2xl bg-gradient-to-r from-yellowF to-yellowT p-[2px] shadow-yellowLi transition-all">
-            <div
-              className={`flex h-full w-full items-center justify-center gap-6 rounded-2xl bg-bg p-4 md:gap-2`}
-            >
-              <div className="bg-gradient-to-r from-yellowF to-yellowT bg-clip-text p-2 text-5xl text-transparent">
-                {data.totalPrize}
-              </div>
-            </div>
-          </div>
         </div>
       </Section>
 

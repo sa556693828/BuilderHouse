745f3e1109f22a24a70a552b07d9776e7715e21c
eeinss <eins@builderdao.co>
2023-07-16 05:15:52 +0800

    activity

--- src/Document/Events/index.tsx
+++ src/Document/Events/index.tsx
@@ -1,8 +1,23 @@
 import eventPic from "@/assets/events1.svg";
 import eventPic0 from "@/assets/events0.png";
+import lead0719 from "@/assets/lead0719.png";
+import trader0719 from "@/assets/trader0719.png";
 
 //新增link即可點擊
 const EventsContent = [
+  {
+    pic: eventPic.src,
+    date: `JULY 19,2023`,
+    title: "LeadUp The Night: 區塊鏈與 AI",
+    content: `重塑資產管理的未來`,
+    link: "https://lu.ma/LeadUpTheNight_9",
+  },  {
+    pic: eventPic.src,
+    date: `JULY 19,2023`,
+    title: "Trader House",
+    content: `量化101，策略交易只有 CTA？`,
+    link: "https://lu.ma/TraderHouse_1",
+  },
   {
     pic: eventPic0.src,
     date: `JUNE 14,2023`,

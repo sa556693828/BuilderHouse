d1f0d1f6224da4f8f1fd58299863903d14b99095
sa556693282 <sa556693828@gmail.com>
2023-08-11 19:24:57 +0800

    new

--- src/components/Agenda/Agenda.tsx
+++ src/components/Agenda/Agenda.tsx
@@ -13,7 +13,7 @@ export default function Agenda(props: { data: any }) {
     >
       <div className="flex w-4/5 flex-col">
         <div className="h-full w-full bg-gradient-to-r from-blueF to-blueT pb-[2px] transition-all">
-          <div className="flex w-full bg-bg pb-10 text-2xl">
+          <div className="flex w-full bg-bg pb-6 text-2xl">
             <div className="w-1/5">
               <div className="text-center">TIME</div>
             </div>
@@ -34,7 +34,7 @@ export default function Agenda(props: { data: any }) {
               </div>
             </div>
             <div className="flex w-2/5 items-center justify-center">
-              <div className="text-center text-2xl font-semibold">
+              <div className="text-center text-xl font-semibold">
                 {data.topic}
               </div>
             </div>

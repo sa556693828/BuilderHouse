6fde7308998606e407a4e89c8627b7e04ea929df
sa556693282 <sa556693828@gmail.com>
2023-08-11 18:50:53 +0800

    update

--- src/components/Agenda/Agenda.tsx
+++ src/components/Agenda/Agenda.tsx
@@ -12,28 +12,37 @@ export default function Agenda(props: { data: any }) {
       id={data.id}
     >
       <div className="flex w-4/5 flex-col">
-        <div className="flex w-full border-b pb-10">
-          <div className="w-2/5">
-            <div className="text-center">TIME</div>
-          </div>
-          <div className="w-3/5">
-            <div className="text-center">TOPIC</div>
+        <div className="h-full w-full bg-gradient-to-r from-blueF to-blueT pb-[2px] transition-all">
+          <div className="flex w-full bg-bg pb-10">
+            <div className="w-1/5">
+              <div className="text-center">TIME</div>
+            </div>
+            <div className="w-2/5">
+              <div className="text-center">TOPIC</div>
+            </div>
+            <div className="w-2/5">
+              <div className="text-center">SPEAKER</div>
+            </div>
           </div>
         </div>
         {data.data?.map((data: any, index: any) => (
-          <div className="flex w-full border-b py-10" key={index}>
-            <div className="w-2/5 pl-20">
-              <div className="flex items-center gap-4 font-normal">
+          <div className="flex w-full border-b py-6" key={index}>
+            <div className="flex w-1/5 justify-center">
+              <div className="flex items-center justify-center gap-4 font-normal">
                 <LuClock3 size="22" />
                 {data.time}
               </div>
             </div>
-            <div className="w-3/5 space-y-2 pl-20">
-              <div className="text-2xl font-semibold">{data.topic}</div>
+            <div className="flex w-2/5 items-center justify-center">
+              <div className="text-center text-2xl font-semibold">
+                {data.topic}
+              </div>
+            </div>
+            <div className="w-2/5">
               {data.subTitle &&
                 data.subTitle.map((sub: any, index: any) => (
                   <div
-                    className="whitespace-pre-wrap text-base font-normal text-text"
+                    className="whitespace-pre-wrap text-center text-base font-semibold text-text"
                     key={index}
                   >
                     {sub}

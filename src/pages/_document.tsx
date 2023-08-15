448ac4d65320f78c609fda0ff5c1e67086beef92
sa556693282 <sa556693828@gmail.com>
2023-08-15 13:34:47 +0800

    ＧＴＭ

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,13 @@
 import theme from "@/components/theme";
 import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const gaScript = `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WQPD7JS2');`;
 
 export default function Document() {
   return (
@@ -27,7 +34,6 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="stylesheet" href="odometer-theme-default.css" />
-
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#13294b" />
         <meta property="og:title" content="Taiwan Builder House" />
@@ -78,11 +84,20 @@ export default function Document() {
             src="https://www.facebook.com/tr?id=3420282974892057&ev=PageView&noscript=1"
           />
         </noscript>
+        <Script id="google-manager">{gaScript}</Script>
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WQPD7JS2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </Html>
   );
